7aa206e8b6663d37996bc2e2c611dd1d7cdd107a
Roosvelt <anaguesonnaroosvelt@gmail.com>
2024-02-15 16:18:23 -0800

    show and delete contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\ContactRequest;
 use Spatie\QueryBuilder\QueryBuilder;
 use App\Http\Resources\ContactCollection;
+use App\Http\Resources\ContactResource;
 
 class ContactController extends Controller
 {
@@ -31,6 +32,13 @@ class ContactController extends Controller
 
 
 
+    /**
+     * Record the informations's user in database and send email to admin's email.
+     * 
+     * @param ContactRequest $request
+     * @return \Illuminate\Http\Response
+     */
+
     public function contact(ContactRequest $request)
     {
         Mail::send(new ContactMail($request->validated()));
@@ -40,5 +48,33 @@ class ContactController extends Controller
         return response()->json([
             "status_message" => "your email has been send successfully"
         ], 201);
+    }
+
+
+    /**
+     * Display the specified contact.
+     *
+     * @param Contact $contact
+     * @return ContactResource
+     */
+    public function show(Contact $contact)
+    {
+        return new ContactResource($contact);
+    }
+
+
+
+
+    /**
+     * Remove the specified contact from storage.
+     *
+     * @param Request $request
+     * @param Contact $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Contact $contact)
+    {
+        $contact->delete();
+        return response()->noContent();
     }
 }
